d6c74a12a12b7c588699c2168413b2e15045f295
cloudecoder <sandeepnet45@gmail.com>
2021-06-01 15:50:41 +0530

    distance prog

---
+++ function.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+## declare a function
+
+echo -e "\e[32m CALCULATE YOUR DISTANCE BY DEFINING SPEED & TIME \e[0m"
+read -p "ENTER THE SPEED : " speed
+read -p "ENTER THE TIME : " time
+
+
+distance() {
+  echo -e "\e[33m YOUR DISTANCE IS CALCULATED SUCCESSFULLY \e[0m"
+  distance= ${speed * time}
+  echo -e "\e[35m distance = $distance"
+
+}
+
+distance
