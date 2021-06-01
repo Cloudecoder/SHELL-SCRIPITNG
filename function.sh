12255177ac6601c6b1d44ea3be051a413a30a555
cloudecoder <sandeepnet45@gmail.com>
2021-06-01 15:56:12 +0530

    distance

--- function.sh
+++ function.sh
@@ -11,7 +11,7 @@ read -p "ENTER THE TIME : " time
 distance() {
   echo -e "\e[33m YOUR DISTANCE IS CALCULATED SUCCESSFULLY \e[0m"
   distance= ${speed * time}
-  echo -e "\e[35m distance \e[0m= " $distance"
+  echo -e "\e[35m Your distance = $distance \e[0m"
 
 }
 
