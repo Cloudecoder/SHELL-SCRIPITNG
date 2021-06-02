c3294ecab84fc84b51b1a1b4bd5881cad2d00157
cloudecoder <sandeepnet45@gmail.com>
2021-06-02 20:06:59 +0530

    common

--- ROBOSHOP/components/common.sh
+++ ROBOSHOP/components/common.sh
@@ -2,6 +2,14 @@ HEAD() {
   echo -n -e "\e[1m $1 \e[0m \t\t...\e[0m"
 }
 
+STAT() {
+  if[ $1 -eq 0]; then
+    echo -e "\e[1;32m done\e[0m"
+  else
+    echo -e "\e[1;31m Failed\e[0m"
+  fi
+}
+
 FAIL() {
   echo -e "\e[1;31mFailed\e[0m"
 }
--- ROBOSHOP/components/frontend.sh
+++ ROBOSHOP/components/frontend.sh
@@ -4,7 +4,7 @@ source components/common.sh
 
 HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[32mDone\e[0m"
+
 
 
 
