b9c2f6b039618a9d78754162c4f861e0d653b2af
cloudecoder <sandeepnet45@gmail.com>
2021-06-02 19:54:11 +0530

    new print

---
+++ ROBOSHOP/components/common.sh
@@ -0,0 +1,12 @@
+HEAD() {
+  echo -n -e "\e[1m $1 \e[0m \t\t..."
+}
+
+FAIL() {
+
+}
+
+SUCC() {
+echo -e "\e[1;32mdone\e[0m"
+
+}
--- ROBOSHOP/components/frontend.sh
+++ ROBOSHOP/components/frontend.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+source components/common.sh
+
+HEAD "Installing Nginx"
+yum install nginx -y &>>/tmp/roboshop.log
+echo -e "\e[32mDone\e[0m"
+
+
+
+
+
+
 echo -e "\e[1;32m Installing frontend\e[0m"
 rm -f /tmp/roboshop.log
 
-yum install nginx -y &>>/tmp/roboshop.log
