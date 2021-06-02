c801bedd6ec7a0455ce6fca1b64d2bc3d06e5ed9
cloudecoder <sandeepnet45@gmail.com>
2021-06-02 15:46:22 +0530

    function

---
+++ impCommands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sample () {
+  echo the value of sample function
+  echo value of a = $a
+  b=20
+  echo first argument =$1
+
+}
+ a=10
+ sample
+ sample xyz
+ echo value of b = $b
