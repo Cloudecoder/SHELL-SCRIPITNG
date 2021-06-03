c8e839ab3e4cea1e58785134f602bac9dc42b158
cloudecoder <sandeepnet45@gmail.com>
2021-06-03 18:03:14 +0530

    frontend commit

--- ROBOSHOP/components/frontend.sh
+++ ROBOSHOP/components/frontend.sh
@@ -12,7 +12,25 @@ systemctl start nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Download from git"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
 
+HEAD "delete old htdocs"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+HEAD "unzip file"
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
+cd /usr/share/nginx/html
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master
+STAT $?
+
+
+#
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
 
