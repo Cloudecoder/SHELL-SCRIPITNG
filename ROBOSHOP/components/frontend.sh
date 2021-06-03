da4bb8cfc874ea533c262c4784252677d13f6abc
cloudecoder <sandeepnet45@gmail.com>
2021-06-03 17:45:22 +0530

    frontend commit

--- ROBOSHOP/components/frontend.sh
+++ ROBOSHOP/components/frontend.sh
@@ -7,6 +7,12 @@ HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Starting Nginx"
+systemctl start nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
+STAT $?
+
+
 
 
 
