c33a69f4ede50246115b7e98cfff5b5361e5f8ea
cloudecoder <sandeepnet45@gmail.com>
2021-06-03 17:25:25 +0530

    file

--- new_script.sh
+++ new_script.sh
@@ -6,3 +6,20 @@ echo -e "\e[33m HElLO /n WELCOME TO THE WORLD OF SANDEEP \e[0m"
 date=$(date +%F)
 
 echo -e "so today date is ${date}"
+
+
+read -p "ENTER THE USERNAME :" username
+
+if["$username" == "root" ]; then
+  echo -e "Hey the $username is a admin user"
+else
+  echo -e "you need to update the $username to root "
+fi
+
+read -p "Enter th file name: " filename
+
+if[ -f "$filename"]; then
+  echo -e "file exists"
+else
+  echo -e "file not found"
+fi
