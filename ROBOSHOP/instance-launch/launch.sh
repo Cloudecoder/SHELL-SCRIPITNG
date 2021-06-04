26931b7253cb835b20a7660a72a8b852eb002668
cloudecoder <sandeepnet45@gmail.com>
2021-06-04 22:48:58 +0530

    lsunch

---
+++ ROBOSHOP/instance-launch/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+component=$1
+
+if [ -z "${component}" ]; then
+  echo "Enter the component to start"
+  exit 1
+fi
+
+LTid=lt-0dde63c285c407ba5
+ver=1
+
+## To launch the template without name
+##aws ec2 run-instances --launch-template LaunchTemplateId=lt-0dde63c285c407ba5,Version=1
+
+##To launch the instance with name
+aws ec2 run-instances --launch-template LaunchTemplateId=${LTid},Version=${ver} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}]"
+
