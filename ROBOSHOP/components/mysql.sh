fbd7c1731727a7619e1477a8dc4d66b1e09b75b3
cloudecoder <sandeepnet45@gmail.com>
2021-06-07 15:36:38 +0530

    my sql update

--- ROBOSHOP/components/mysql.sh
+++ ROBOSHOP/components/mysql.sh
@@ -9,7 +9,7 @@ echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
-gpgcheck=0' > /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 STAT $?
 
 HEAD "Installing mYSQl"
