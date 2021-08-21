01df7e5cc95e67ba86d328ebb98db345f3255ad0
cloudecoder <sandeepnet45@gmail.com>
2021-08-22 01:05:57 +0530

    table

--- shell.sh
+++ shell.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 number=5
-
 for (( i = 1; i <= 10; i++ )); do
-    echo "$number x $i =" $((number * i))
-done
-
-
-a=1
-while [ $a -le 10 ]; do
-  echo "$number x $a = $((number * a))"
-  a=$((a+1))
-
+    echo "$number X $i= $((number*i))"
 done
 
 
