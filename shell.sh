7d1380341c1d6adf0fbeaf60628d016f5fcede46
cloudecoder <sandeepnet45@gmail.com>
2021-08-22 00:54:57 +0530

    table

--- shell.sh
+++ shell.sh
@@ -2,9 +2,9 @@
 
 number=5
 
-for (( i = 1; i <= 10; i++ )); do
-    echo "$number x $i =" $((number * i))
-done
+#for (( i = 1; i <= 10; i++ )); do
+#    echo "$number x $i =" $((number * i))
+#done
 
 
 a=1
