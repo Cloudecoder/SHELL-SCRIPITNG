0c41eba82cb110754717b4e87ee999728c5ed0a8
cloudecoder <sandeepnet45@gmail.com>
2021-08-22 01:02:06 +0530

    table

--- shell.sh
+++ shell.sh
@@ -2,15 +2,15 @@
 
 number=5
 
-#for (( i = 1; i <= 10; i++ )); do
-#    echo "$number x $i =" $((number * i))
-#done
+for (( i = 1; i <= 10; i++ )); do
+    echo "$number x $i =" $((number * i))
+done
 
 
 a=1
-while [ $a -eq 10 ]; do
-  echo "$number x $a =" $((number * a))
-  a=$a+1
+while [ $a -le 10 ]; do
+  echo "$number x $a = $((number * a))"
+  a=$((a+1))
 
 done
 
